03ae0fe2bf85c103ddf5cdce5a328623e53aa653
rajeshmeka <129838861+rajeshmeka@users.noreply.github.com>
2023-04-21 18:28:04 +0800

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "this is a sample web app34";
+        return "this is a sample web app77";
     }
 
 }
